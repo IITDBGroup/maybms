f832972cde0574858709a7145ebc4cfb77ff9372
Boris Glavic <lordpretzel@gmail.com>
2020-07-01 18:47:38 -0500

    installpip

--- docker/installPipFuncs.sh
+++ docker/installPipFuncs.sh
@@ -10,8 +10,8 @@ ${INSTALLBINDIR}/pg_ctl -D ${DATADIR} -w start
 sleep 10
 ####################
 echo - create testdb
-${INSTALLBINDIR}/psql -h localhost -p 5432 -U $PGUSER -d template1 -f /home/maybms/src/pip_plugin/pip.sql
-${INSTALLBINDIR}/psql -h localhost -p 5432 -U $PGUSER -d template1 -f /home/maybms/src/pip_plugin/pip.sql
+${INSTALLBINDIR}/psql -h localhost -p 5432 -U $PGUSER -d maybms -f /home/maybms/src/pip_plugin/pip-noctype.sql
+${INSTALLBINDIR}/psql -h localhost -p 5432 -U $PGUSER -d template1 -f /home/maybms/src/pip_plugin/pip-noctype.sql
 ####################
 echo - shutdown server
 ${INSTALLBINDIR}/pg_ctl -w stop
