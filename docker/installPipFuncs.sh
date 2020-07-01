ff3ca7e7f136dee97e7b8ff5082b1484526b7cc8
Boris Glavic <lordpretzel@gmail.com>
2020-07-01 16:56:23 -0500

    docker

---
+++ docker/installPipFuncs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+PGUSER=$1
+MAYBMSHOME=/home/maybms
+SRCDIR=${MAYBMSHOME}/src
+INSTALLBINDIR=${MAYBMSHOME}/install/bin
+DATADIR=${MAYBMSHOME}/datadir
+####################
+echo - start server
+${INSTALLBINDIR}/pg_ctl -D ${DATADIR} -w start
+sleep 10
+####################
+echo - create testdb
+${INSTALLBINDIR}/psql -h localhost -p 5432 -U $PGUSER -d template1 -f /home/maybms/src/pip_plugin/pip.sql
+${INSTALLBINDIR}/psql -h localhost -p 5432 -U $PGUSER -d template1 -f /home/maybms/src/pip_plugin/pip.sql
+####################
+echo - shutdown server
+${INSTALLBINDIR}/pg_ctl -w stop
